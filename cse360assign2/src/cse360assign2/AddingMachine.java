7e38c917f2860f74d67ba9c96d5258d479f5d85d
angelicainjejikian <angelicainjejikian@100.64.21.83>
2019-10-09 21:25:14 -0700

    Initial commit of project.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,77 @@
+ /*
+ * Author: Angelica Injejikian
+ * Class ID: 85141
+ * Assignment #: Assignment #2
+ * Description: This class will includes various methods that
+ * 				can add and subtract values and can return
+ * 				a string demonstrating the calculations/
+ */
+
+package cse360assign2;
+
+/*
+ * This class will conduct addition and subtraction calculations
+ * and can print the total as well as a string demonstrating the
+ * calculations.
+ * 
+ * @param	total	the integer that contains the total of the calculations
+ */
+
+public class AddingMachine 
+{
+	private int total;
+	
+	
+	/*
+	 * This is a constructor.
+	 */
+	public AddingMachine () 
+	{
+		
+	}
+	
+	/*
+	 * This method returns the current total.
+	 * 
+	 * @return 			the current total
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/*
+	 * This method adds the parameter to the total value.
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/*
+	 * This method subtracts the parameter from the total value.
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/*
+	 * This method returns a history of transactions.
+	 * 
+	 * @return		a string containing a history of the transactions
+	 * @see 		java.lang.Object#toString()
+	 */
+	public String toString () 
+	{
+		return "";
+	}
+
+	/*
+	 * This method will clear the history.
+	 */
+	public void clear() 
+	{
+	
+	}
+}
